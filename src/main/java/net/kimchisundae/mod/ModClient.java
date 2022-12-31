de91495a2bf79efc1b08a58c2daf257077b52bdc
KimchiSundae <aabbior47@gmail.com>
2022-12-31 17:17:07 +0900

    Initial commit

---
+++ src/main/java/net/kimchisundae/mod/ModClient.java
@@ -0,0 +1,11 @@
+package net.kimchisundae.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
